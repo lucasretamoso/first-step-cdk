bdc06841423484d0424523e32bf3471fe715a127
lucasretamoso <lucas.retamoso@whiteprompt.com>
2022-08-05 17:41:11 -0300

    feat: add api

--- lib/first-step-cdk-stack.ts
+++ lib/first-step-cdk-stack.ts
@@ -1,19 +1,38 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { RestApi, Cors, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
 export class FirstStepCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambdaFunction = new NodejsFunction(this, 'lamdbaId', {
+    const lambdaFunction = new NodejsFunction(this, 'lambdaId', {
       functionName: 'sum_numbers',
       description: 'example',
-      memorySize: 32,
+      memorySize: 256,
       runtime: Runtime.NODEJS_16_X,
       entry: 'code/sum_numbers/index.ts',
       handler: 'main'
     })
+
+    const api = new RestApi(this, 'restApiID', {
+      restApiName: 'first_step_cdk_api',
+      deployOptions: {
+        stageName: 'staging',
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: Cors.DEFAULT_HEADERS,
+      },
+    })
+
+    const resourceSumNumbers = api.root.addResource('sum_numbers')
+
+    const integrations = new LambdaIntegration(lambdaFunction)
+
+    resourceSumNumbers.addMethod('POST', integrations);
   }
 }
