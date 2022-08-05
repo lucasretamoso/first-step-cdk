eb8dca13059f263fa96193958967a23b1361a622
lucasretamoso <lucas.retamoso@whiteprompt.com>
2022-08-05 10:44:30 -0300

    init commit

---
+++ test/first-step-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FirstStepCdk from '../lib/first-step-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/first-step-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FirstStepCdk.FirstStepCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
